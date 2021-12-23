60a5810c084c63e9cff317d7efc8fa36013514d9
seenso <>
2021-12-23 10:08:06 -0900

    Completed assignmetn

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -4,14 +4,29 @@ class Dev < ActiveRecord::Base
 
   def received_one?(item_name)
     #accepts an item_name (string) and returns true if any of the freebies associated with the dev has that item_name, otherwise returns false
-    self.freebies.any? do |freebie|
-      freebie.item_name == item_name
-    end
+    
+    #one way to write this method
+    self.freebies.find_by(item_name: item_name)
+
+    #another way
+    # self.freebies.any? do |freebie|
+    #   freebie.item_name == item_name
+    # end
   end
 
   def give_away(dev, freebie)
     #accepts a Dev instance and a Freebie instance, changes the freebie's dev to be the given dev; your code should only make the change if the freebie belongs to the dev who's giving it away
-    freebie.update(dev: dev) unless freebie.dev != self
+
+    #Ruby shorthand way
+    # freebie.update(dev: dev) unless freebie.dev != self
+
+    #if/else logic way
+    if self == freebie.dev
+      freebie.dev = dev
+      freebie.save
+    else
+      "#{freebie.item_name} is not yours to give!"
+    end
   end
 
 end
